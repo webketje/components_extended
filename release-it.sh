ffd051ba0446a48ebaf74ca4e7b46d7421a0dac4
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2019-04-08 00:27:11 +0200

    Fix shell script

--- release-it.sh
+++ release-it.sh
@@ -1,7 +1,6 @@
 echo Preparing release
 read -p "Release version: " version
-read -p "Archive name: " archive_name
-git archive --format zip -o "components_extended_${version}.zip" -4 master
+git archive --format zip -o "components_extended_${version}.zip" -4 master:plugin
 
 read -p "Create github release? (y/n)" release
 
